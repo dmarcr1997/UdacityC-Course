d9d0502793fa19bf39e340a2fa626c35234b2ccc
Damon <dmarcr1997@gmail.com>
2022-04-07 13:32:49 -0400

    040722 - Board Vector Setup

---
+++ mapTracker/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<vector<int>> board
+    {{0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0}};
+    
+}
