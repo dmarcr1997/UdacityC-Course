ce7187ad62e918f48d93a3c26b7b4d05a46c12a9
Damon <dmarcr1997@gmail.com>
2022-04-08 14:43:27 -0400

    04/08/2022 - Added file reading functionality and vector reading function

--- mapTracker/main.cpp
+++ mapTracker/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
+
+
+void ReadBoardFile(string path){
+	ifstream board_file(path);
+    if(board_file){
+        cout << "The file stream has been created!" << endl;
+        string line;
+        while(getline(board_file, line)){
+            cout << line << endl;
+        }
+    }
+}
+
+void PrintBoard(vector<vector<int>> board){
+	for(auto row: board){
+        for(int val: row){
+            cout << val;  
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     vector<vector<int>> board
@@ -9,5 +32,7 @@ int main(){
     {0, 1, 0, 0, 0, 0},
     {0, 1, 0, 0, 0, 0},
     {0, 0, 0, 0, 1, 0}};
-    
+    PrintBoard(board);
+    string path = "./board1.txt";
+    ReadBoardFile(path);
 }
