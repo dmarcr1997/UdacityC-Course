5f3084aa42abf5cf15314e181c90dd7806827279
Damon <dmarcr1997@gmail.com>
2022-04-11 15:34:05 -0400

    04/11/2022 - #5 - Added complex board

--- mapTracker/main.cpp
+++ mapTracker/main.cpp
@@ -143,7 +143,7 @@ void PrintBoard(const vector<vector<State>> board) {
 int main() {
     int init[2]{0, 0};
     int goal[2]{4, 5};
-    auto board = ReadBoardFile("./board1.txt");
+    auto board = ReadBoardFile("./board2.txt");
     auto solution = Search(board, init, goal);
     PrintBoard(solution);
 }
